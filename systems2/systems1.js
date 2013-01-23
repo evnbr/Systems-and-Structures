fb894a2c46f8a7ca52b6731b4fcd845372c0c810
Evan <brooks.ev@gmail.com>
2013-01-23 17:30:50 -0500

    Added gtfs project

--- systems2/systems1.js
+++ systems2/systems1.js
@@ -1,34 +1,4 @@
 var alfa = "abcdefghijklmnopqrstuvwxyz";
-var vid = "x0chehsfbc52kb32sbed38v3d3w2ujhsjuayb2enek";
-var letterFreq = {
-	a : 8.167,
-	b : 1.492,	
-	c : 2.782,	
-	d : 4.253,	
-	e : 12.702,
-	f : 2.228,
-	g : 2.015,
-	h : 6.094,
-	i : 6.966,
-	j : 0.153,
-	k : 0.772,
-	l : 4.025,
-	m : 2.406,
-	n : 6.749,
-	o : 7.507,
-	p : 1.929,
-	q : 0.095,
-	r : 5.987,
-	s : 6.327,
-	t : 9.056,
-	u : 2.758,
-	v : 0.978,
-	w : 2.360,
-	x : 0.150,
-	y : 1.974,
-	z : 0.074,
-}
-
 
 $(function(){
 	$("#start").click(refresh);
@@ -72,97 +42,6 @@ $(function(){
 				$("#output").append("<span class=\"divide\">–</span>");
 			}
 		}
-
-		for (j = 0; j < words.length; j++ ){
-			chars = words[j].split('');
-
-			//$("#output").text(getFirst(chars).let);
-			//console.log(chars.sort());
-
-			var scrambledWords = new Array();
-
-			// for (i = 0; i < chars.length; i++) {
-			// 	var charsRemaining = chars.slice(0); // slice(0) copies!
-			// 	var begin = chars[i];
-			// 	removeA(charsRemaining, begin);
-			// 	console.log(begin);
-			// 	console.log(charsRemaining);
-
-			// 	for (k = 0; k < charsRemaining.length; k++) {
-			// 		var charsRemaining = charsRemaining.slice(0); // slice(0) copies!
-			// 		var begin = charsRemaining[k];
-			// 		removeA(charsRemaining, begin);
-			// 		console.log(begin);
-			// 		console.log(charsRemaining);
-			// 	}
-			// 	console.log("----");
-			// }
-
-
-			// Hooray! I wrote my dirst recursive thing!
-
-			// // ----------------------
-			// // ----------------------
-
-			// for (iX[l] = 0; iX[l] < chars.length; iX[l]++) {
-			// 	currLet = chars[iX[l]]
-			// 	stubs[l] = currLet;
-			// 	console.log(stubs[l]);
-			// 	remainLetArr[l] = removeA(chars.slice(0), currLet);
-
-
-			// 	// ---------
-			// 	l++;
-			// 	// ---------
-			// 	for (iX[l] = 0; iX[l] < remainLetArr[l-1].length; iX[l]++) {
-			// 		stubs[l] = stubs[l-1] + remainLetArr[l-1][iX[l]];
-			// 		console.log(stubs[l]);
-			// 		remainLetArr[l] = removeA(remainLetArr[l-1].slice(0), remainLetArr[l-1][iX[l]]);
-
-			// 		// ---------
-			// 		l++;
-			// 		// ---------
-			// 		for (iX[l] = 0; iX[l] < remainLetArr[l-1].length; iX[l]++) {
-			// 			stubs[l] = stubs[l-1] + remainLetArr[l-1][iX[l]];
-			// 			console.log(stubs[l]);
-			// 			remainLetArr[l] = removeA(remainLetArr[l-1].slice(0), remainLetArr[l-1][iX[l]]);
-
-
-			// 			// ---------
-			// 			l++;
-			// 			// ---------
-			// 			for (iX[l] = 0; iX[l] < remainLetArr[l-1].length; iX[l]++) {
-			// 				stubs[l] = stubs[l-1] + remainLetArr[l-1][iX[l]];
-			// 				console.log(stubs[l]);
-			// 				if ( l >= maxLevels) {
-			// 					fullWords.push(stubs[l]);
-			// 				}
-			// 				else {
-			// 					// keep going
-			// 				}
-			// 			}
-			// 			// ---------
-			// 			l--;
-			// 			// ---------
-			// 		}
-			// 		// ---------
-			// 		l--;
-			// 		// ---------
-			// 	}
-			// 	// ---------
-			// 	l--;
-			// 	// ---------
-
-			// 	console.log("-----");
-			// }
-
-			// var fullWords = shuffleLetters(chars);
-
-			// for (i = 0; i < fullWords.length; i++){
-			// 	$("#output").append(fullWords[i] + "</br>");
-			// }
-		}
-
 
 	};
 
@@ -263,31 +142,6 @@ $(function(){
 		}
 
 		return fullWords;
-	}
-
-	function getFirst(charArray) {
-
-		sorted = charArray.sort();
-		return sorted[0];
-
-		// var minIndex = 1000000;
-		// var firstLetter = -1;
-
-		// for (i = 0; i < charArray.length; i++) {
-		// 	currLetter = charArray[i];
-		// 	currIndex = alfa.indexOf(currLetter);
-		// 	if (currIndex < minIndex) {
-		// 		firstLetter = currLetter;
-		// 		minIndex = currIndex;
-		// 	}
-		// }
-
-		// var result = {
-		// 	let: firstLetter,
-		// 	index: minIndex
-		// }
-
-		// return result;
 	}
 
 	function removeA(array, item){
